a5abf64f13b71cee8b8009b0d3042e212cd8be74
Alan Rios <alan.rios.hd@gmail.com>
2024-10-25 14:34:41 -0500

    Add cordova-plugin-file dependency

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  templateUrl: './tab3.page.html',
+  styleUrls: ['./tab3.page.scss'],
 })
 export class Tab3Page {
+  description: string = '';
+  savedDescription: string | null = null;
 
   constructor() {}
 
+  saveDescription() {
+    this.savedDescription = this.description;
+    this.description = '';
+  }
 }
